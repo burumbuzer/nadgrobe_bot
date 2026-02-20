4b69842397fc4738d72212fb0e6b768af9e5528f
burumbuzer <aarbuzak@gmail.com>
2026-02-20 15:07:42 +0700

    добавил словари в формате, йо

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from telebot import types, TeleBot
 
-from pole import PoleGame
+from pole import *
 
 import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-bot = TeleBot(config["Telegram"]["TOKEN"].replace("'", "").replace('"', ''))
+bot = TeleBot(config["Telegram"]["TOKEN"])
 
 CURRENT_GAMES = {}
 
@@ -16,9 +16,9 @@ def start_game_handler(message: types.Message):
   '''
   Начало игры, отзывается на /start и /play
   '''
-  game = PoleGame()
+  game = PoleGame("test")
   CURRENT_GAMES[message.chat.id] = game
-  bot.send_message(message.chat.id, text=f"Начало игры!\n\n{game.printWord()}")
+  bot.send_message(message.chat.id, text=f"Начало игры!\n\n{game.print_word()}")
 
 
 @bot.message_handler(func=lambda message: message.chat.id in CURRENT_GAMES, content_types=["text"])
@@ -32,17 +32,17 @@ def process_game_handler(message: types.Message):
     return
   
   if len(message.text) == 1:
-    if game.checkLetter(str.lower(message.text)) and not game.end:
-      bot.send_message(message.chat.id, f"Буква {str.upper(message.text)} есть в слове!\n\n{str.upper(game.printWord())}")
-    elif not game.checkLetter(str.lower(message.text)):
-      bot.send_message(message.chat.id, f"Буквы {str.upper(message.text)} нет в слове!\n\n{str.upper(game.printWord())}")
+    if game.check_letter(str.lower(message.text)) and not game.end:
+      bot.send_message(message.chat.id, f"Буква {str.upper(message.text)} есть в слове!\n\n{str.upper(game.print_word())}")
+    elif not game.check_letter(str.lower(message.text)):
+      bot.send_message(message.chat.id, f"Буквы {str.upper(message.text)} нет в слове!\n\n{str.upper(game.print_word())}")
   elif len(message.text) > 1:
     if len(str.split(message.text)) == 1:
-      if not game.checkWord(str.lower(message.text)):
-        bot.send_message(message.chat.id, f"Слово {str.upper(message.text)} неверное!\n\n{str.upper(game.printWord())}")
+      if not game.check_word(str.lower(message.text)):
+        bot.send_message(message.chat.id, f"Слово {str.upper(message.text)} неверное!\n\n{str.upper(game.print_word())}")
 
   if game.end:
-    bot.send_message(message.chat.id, f"Конец игры!\n\nЗагаданное слово: {str.upper(game.printWord())}\n\nПлюс 52 балла юзеру @{message.from_user.username}!")
+    bot.send_message(message.chat.id, f"Конец игры!\n\nЗагаданное слово: {str.upper(game.print_word())}\n\nПлюс 52 балла юзеру @{message.from_user.username}!")
     del CURRENT_GAMES[message.chat.id]
 
 
--- pole.py
+++ pole.py
@@ -1,18 +1,28 @@
 import random
+import configparser
+INI_DICTIONARIES = configparser.ConfigParser()
+INI_DICTIONARIES.read("dictionaries.ini")
 
-DICT = ["манул", "ирбис", "горностай"]
+DICTIONARIES = {}
+for d in list(INI_DICTIONARIES.keys())[1:]: # первым элементов конфиг файла ВСЕГДА будет раздел DEFAULTS, в котором у нас ничего быть не должно, вот так вот
+  with open(f"dicts\{INI_DICTIONARIES[d]['file']}", "r", encoding="utf-8") as file: # логгер + обработка ошибок
+    process_dict = file.readlines()
+  process_dict = [w.strip("\n") for w in process_dict]
+  DICTIONARIES[d] = process_dict
 
 class PoleGame():
   word: str
   guessedLetters: list[str]
   end: bool
 
-  def __init__(self):
-    self.word = random.choice(DICT)
+  def __init__(self, dict_name: str):
+    if dict_name not in DICTIONARIES:
+      pass # логгер + обработка ошибок
+    self.word = random.choice(DICTIONARIES[dict_name])
     self.guessedLetters = []
     self.end = False
 
-  def checkLetter(self, letter: str) -> bool:
+  def check_letter(self, letter: str) -> bool:
     if letter in self.word:
       self.guessedLetters.append(letter)
       if len(self.word) == len(self.guessedLetters):
@@ -21,7 +31,7 @@ class PoleGame():
     else:
       return False
   
-  def checkWord(self, word: str) -> bool:
+  def check_word(self, word: str) -> bool:
     if str.lower(word)==str.lower(self.word):
       self.end = True
       self.guessedLetters = [c for c in self.word]
@@ -29,5 +39,5 @@ class PoleGame():
     else:
       return False
     
-  def printWord(self) -> str:
+  def print_word(self) -> str:
     return "".join(c if c in self.guessedLetters else "■" for c in self.word)
